822586f81b326ecd664f5aacc748831a41908be8
ValLaroche <35736633+ValLaroche@users.noreply.github.com>
2024-07-10 13:49:48 +0200

    Delete scripts directory

--- scripts/Clustering/model.R
+++
@@ -1,8 +0,0 @@
-# Generate a consensus clustering model with given arguments
-CC_modelization <- function(datasets, title_model, cluster_alg = "hc", distance = "euclidean"){
-  CC_model = ConsensusClusterPlus(datasets, maxK = 6, reps = 50, pItem = 0.8, pFeature = 1,
-                                  clusterAlg = cluster_alg, distance = distance, 
-                                  innerLinkage="ward.D2", finalLinkage="ward.D2",
-                                  seed = 126, plot = "pdf", title = title_model)
-  return(CC_model)
-}
